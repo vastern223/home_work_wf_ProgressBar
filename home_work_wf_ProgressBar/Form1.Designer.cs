f82eff5c2844942c0425f2315ba9a565f7971963
Василь Колотюк <vasakolotuk02@gmail.com>
2020-11-01 14:55:40 +0200

    first commit

---
+++ home_work_wf_ProgressBar/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace home_work_wf_ProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(419, 22);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(165, 45);
+            this.trackBar1.TabIndex = 24;
+            this.trackBar1.Value = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(483, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "50";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(383, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Hard";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(590, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Easy";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.statusStrip1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button11);
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Location = new System.Drawing.Point(-1, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(802, 356);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "game";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4});
+            this.statusStrip1.Location = new System.Drawing.Point(3, 331);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(796, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(75, 17);
+            this.toolStripStatusLabel1.Text = "next number";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel2.Text = "0";          
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(59, 17);
+            this.toolStripStatusLabel3.Text = "too much";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel4.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(155, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 36);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(356, 42);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(91, 36);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(459, 144);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(91, 36);
+            this.button11.TabIndex = 10;
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(155, 251);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(91, 36);
+            this.button10.TabIndex = 9;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(548, 42);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(91, 36);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(548, 251);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(91, 36);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(257, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 36);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(356, 251);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(91, 36);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(634, 144);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(91, 36);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(61, 144);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 36);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(633, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 52);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ home_work_wf_ProgressBar/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace home_work_wf_ProgressBar
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            toolStripStatusLabel2.Text = counter.ToString();
+            counters = trackBar1.Value * 10;
+            toolStripStatusLabel4.Text = counters.ToString();
+            timer.Tick += new EventHandler(Show_info);
+        }
+        int counters;
+        int counters_time;
+        int counter = 1;
+
+        private void Show_info(object sender, EventArgs e)
+        {
+            counters_time++;
+            counters--;
+            toolStripStatusLabel4.Text = counters.ToString();
+            if (toolStripStatusLabel2.Text == "11")
+            {
+                timer.Stop();
+                MessageBox.Show($"ти виграв\nзатрачений час: {counters_time} c");
+
+            }
+            else
+            if (counters == 0)
+            {
+                timer.Stop();
+                MessageBox.Show($"you lose");
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Interval = 1000;
+            timer.Start();
+            int[] arr = new int[10];
+
+            Random random = new Random();
+            int rnd;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                rnd = random.Next(1, 11);
+                arr[i] = rnd;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j] && i != j)
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            int k = 0;
+            foreach (Button rb1 in groupBox1.Controls.OfType<Button>())
+            {
+                rb1.Text = arr[k].ToString();
+                k++;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == counter.ToString())
+            {
+                button2.BackColor = Color.Green;
+                    counter++;
+                toolStripStatusLabel2.Text = counter.ToString();
+                toolStripProgressBar1.Value += 10;
+            }
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.Text == counter.ToString())
+            {
+                button8.BackColor = Color.Green;
+                    counter++;
+                toolStripStatusLabel2.Text = counter.ToString();
+                toolStripProgressBar1.Value += 10;
+            }
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button9.Text == counter.ToString())
+            {
+                button9.BackColor = Color.Green;
+                    counter++;
+                toolStripStatusLabel2.Text = counter.ToString();
+                toolStripProgressBar1.Value += 10;
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == counter.ToString())
+            {
+                button5.BackColor = Color.Green;
+                    counter++;
+                toolStripStatusLabel2.Text = counter.ToString();
+                toolStripProgressBar1.Value += 10;
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == counter.ToString())
+            {
+                button4.BackColor = Color.Green;
+                    counter++;
+                toolStripStatusLabel2.Text = counter.ToString();
+                toolStripProgressBar1.Value += 10;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == counter.ToString())
+            {
+                button3.BackColor = Color.Green;
+                    counter++;
+                toolStripStatusLabel2.Text = counter.ToString();
+                toolStripProgressBar1.Value += 10;
+            }
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (button11.Text == counter.ToString())
+            {
+                button11.BackColor = Color.Green;
+                    counter++;
+                toolStripStatusLabel2.Text = counter.ToString();
+                toolStripProgressBar1.Value += 10;
+            }
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button7.Text == counter.ToString())
+            {
+                button7.BackColor = Color.Green;
+                    counter++;
+                toolStripStatusLabel2.Text = counter.ToString();
+                toolStripProgressBar1.Value += 10;
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Text == counter.ToString())
+            {
+                button6.BackColor = Color.Green;
+                    counter++;
+                toolStripStatusLabel2.Text = counter.ToString();
+                toolStripProgressBar1.Value += 10;
+            }
+
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (button10.Text == counter.ToString())
+            {
+                button10.BackColor = Color.Green;
+                    counter++;
+                toolStripStatusLabel2.Text = counter.ToString();
+                toolStripProgressBar1.Value += 10;
+            }
+
+
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            counters = trackBar1.Value * 10;
+            toolStripStatusLabel4.Text = counters.ToString();
+        }
+    }
+}
